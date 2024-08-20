5ce1d5b9ec6956ed2684b891460425c78f6cf8cb
massiveattacker <119751708+massiveattacker@users.noreply.github.com>
2024-08-20 16:20:13 -0300

    estilização básica de botões, index e navbar

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Távola Redonda</title>
-    <link rel="stylesheet" href="/tavola-redonda/css/styles.css">
+    <link rel="stylesheet" href="<?php echo $base_path; ?>/css/styles.css">
     <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $base_path; ?>/assets/icons/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $base_path; ?>/assets/icons/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $base_path; ?>/assets/icons/favicon-16x16.png">
--- includes/nav.php
+++ includes/nav.php
@@ -1,6 +1,6 @@
-<nav>
+<?php $base_path = '/tavola-redonda'; ?>
+<nav class="navbar">
     <a href="<?php echo $base_path; ?>/index.php">Página Principal</a>
     <a href="<?php echo $base_path; ?>/views/index.php">Mesas</a>
     <a href="<?php echo $base_path; ?>/views/add.php">Criar Mesa</a>
 </nav>
-
