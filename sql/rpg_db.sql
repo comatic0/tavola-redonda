415f7216b1abc588a7ff7e5b2b60aca18ade9ca5
massiveattacker <119751708+comatic0@users.noreply.github.com>
2024-09-10 16:21:22 -0300

    db sql file update

--- sql/rpg_db.sql
+++ sql/rpg_db.sql
@@ -1,31 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 10/09/2024 às 20:29
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `rpg_db`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `mesas`
---
 
 CREATE TABLE `mesas` (
   `id` int(11) NOT NULL,
@@ -36,19 +12,10 @@ CREATE TABLE `mesas` (
   `categoria` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Despejando dados para a tabela `mesas`
---
-
 INSERT INTO `mesas` (`id`, `nome`, `descricao`, `nome_do_mestre`, `numero_max_jogadores`, `categoria`) VALUES
 (18, 'Senhor dos Anéis 2', 'Tabletop RPG.', 'Gandalf', 14, 'Fantasia'),
 (30, 'Senhor dos Anéis', 'afawfsadfasdgasdg', 'Mago', 12, 'Fantasia');
 
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `usuarios`
---
 
 CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
@@ -57,39 +24,15 @@ CREATE TABLE `usuarios` (
   `email` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Índices para tabelas despejadas
---
-
---
--- Índices de tabela `mesas`
---
 ALTER TABLE `mesas`
   ADD PRIMARY KEY (`id`);
 
---
--- Índices de tabela `usuarios`
---
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT para tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `mesas`
---
 ALTER TABLE `mesas`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
---
--- AUTO_INCREMENT de tabela `usuarios`
---
 ALTER TABLE `usuarios`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
