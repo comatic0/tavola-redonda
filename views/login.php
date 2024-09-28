d239bfebbaf733327c4b9536266eb7d6d57263c4
massiveattacker <119751708+comatic0@users.noreply.github.com>
2024-09-28 16:29:41 -0300

    login & logout funcional

--- views/login.php
+++ views/login.php
@@ -1,8 +1,32 @@
+<?php
+session_start();
+require '../includes/db.php';
+require '../includes/functions.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $user = getUserByEmail($pdo, $email);
+
+    if ($user && password_verify($password, $user['password'])) {
+        $_SESSION['user_id'] = $user['id'];
+        $_SESSION['username'] = $user['username'];
+        header('Location: ../index.php');
+        exit();
+    } else {
+        $error = "Email ou senha incorretos.";
+    }
+}
+?>
+
 <?php include '../includes/header.php'; ?>
 <?php include '../includes/nav.php'; ?>
-
 <div class="form-container">
     <h2>Logar</h2>
+    <?php if (isset($error)): ?>
+        <p class="error"><?php echo $error; ?></p>
+    <?php endif; ?>
     <form action="login.php" method="post">
         <div class="form-group">
             <label for="email">Email:</label>
@@ -15,5 +39,4 @@
         <button type="submit" class="btn">Logar</button>
     </form>
 </div>
-
 <?php include '../includes/footer.php'; ?>
