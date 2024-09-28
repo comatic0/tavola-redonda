e9dbff0c73b6706e935c269f681f6ddca829c363
massiveattacker <119751708+comatic0@users.noreply.github.com>
2024-09-28 15:32:19 -0300

    torna funcionais os botões da navbar (index)

--- includes/nav.php
+++ includes/nav.php
@@ -1,16 +1,20 @@
 <?php $base_path = '/tavola-redonda'; ?>
 <header class="header">
-        <div class="logo">
-            <img src="<?php echo $base_path; ?>/assets\icons/logo.png" alt="Logo Távula Redonda">
+    <div class="logo">
+        <a href="<?php echo $base_path; ?>/index.php">
+            <img src="<?php echo $base_path; ?>/assets/icons/logo.png" alt="Logo Távula Redonda">
+        </a>
+        <a href="<?php echo $base_path; ?>/index.php" class="title-link">
             <h1>Távola Redonda</h1>
-        </div>
-        <nav class="navbar">
-            <a href="#" class="active">Página Principal</a>
-            <a href= "<?php echo $base_path; ?>/views/index.php">Mesas</a>
-            <a href="#">Fichas</a>
-        </nav>
-        <div class="user">
-            <a href="<?php echo $base_path; ?>/views/register.php">Crie sua conta</a>
-            <img src="<?php echo $base_path; ?>/assets\icons/user-icon.png" alt="User Icon">
-        </div>
+        </a>
+    </div>
+    <nav class="navbar">
+        <a href="<?php echo $base_path; ?>/index.php" class="active">Página Principal</a>
+        <a href="<?php echo $base_path; ?>/views/index.php">Mesas</a>
+        <a href="#">Fichas</a>
+    </nav>
+    <div class="user">
+        <a href="<?php echo $base_path; ?>/views/register.php">Crie sua conta</a>
+        <img src="<?php echo $base_path; ?>/assets/icons/user-icon.png" alt="User Icon">
+    </div>
 </header>
